0c61e44c48d602b6ec3ef34ecda23bb53739fb46
Rafael Tovar <rtovarm@gmail.com>
2012-04-02 18:57:24 -0700

    Signal is done

--- SampleSolution/a2Work/xeros/c/signal.c
+++ SampleSolution/a2Work/xeros/c/signal.c
@@ -3,6 +3,14 @@
 
 #include <kernel.h>
 
+typedef struct sigtramp_frame {
+  context_frame	cf;
+  unsigned int	ret_addrs;
+  unsigned int	handler;
+  unsigned int 	cntx_frame;
+  unsigned int	old_esp;
+} sigtramp_frame;
+
 extern void sigtramp(void(*handler)(void*),void*cntx,void*osp)
 {
 	handler(cntx);
@@ -12,12 +20,29 @@ extern void sigtramp(void(*handler)(void*),void*cntx,void*osp)
 int signal( int pid, int sig_no) {
 /***********************************************/
 
-	//TODO make this method how it should be...
 	pcb *p = findPCB(pid);
 	int num = get_priority_signal(p);
 	if(is_valid_signal(num) && num>(p->current_signal) ){
 	    p->current_signal = num;
 	    //sigtramp(p->signal_handlers[num],(void *)((p->esp)-sizeof(context_frame)), (void *)(p->esp));
+	    sigtramp_frame *fr = (sigtramp_frame  *) (p->esp - sizeof(sigtramp_frame));
+	    //sets everything to 0
+	    memset(fr, 0, sizeof( sigtramp_frame ));
+	    //instruction pointer in the blue frame
+	    fr->cf.iret_eip = (int) &sigtramp;
+	    //yellow frame slide 286	    
+	    fr->ret_addrs = (unsigned int) &sigtramp;
+	    fr->handler = p->signal_handlers[num];
+	    fr->cntx_frame = p->esp;
+	    fr->old_esp = p->esp;
+	    //change stack pointer to point at blue frame	
+	    p->esp = (unsigned int) fr;  
+	    return 1;
+	}
+	return 0;
+}
+
+		/*
 	    int *ptr = (int *)p->esp;
 	    //Old_sp (a3)
 	    ptr--;
@@ -30,17 +55,14 @@ int signal( int pid, int sig_no) {
 	    *ptr= p->signal_handlers[num];
 	    //Return address (never used)
 	    ptr--;
+
 	    *ptr = (int) &sigtramp;
 	    //Blue context frame from slide 286
 	    ptr -= sizeof(context_frame);
 	    //TODO check if this context frame is right	
+
 	    context_frame * cf = (context_frame *) ptr;
 	    cf->iret_eip = (int) &sigtramp;
 	    //change the stack pointer to point at the top of blue frame
 	    p->esp = (int)ptr;  
-	    return 1;
-	}
-	return 0;
-}
-
-
+		*/
